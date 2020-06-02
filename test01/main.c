fa2eebc83c1b54cb2b85e478a8fae125584f4413
Anna Semenova <anya-semenova-2002@mail.ru>
2020-06-02 17:01:29 +0300

    Вторая верися калькулятора

---
+++ test01/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *  Created on: 17 апр. 2020 г.
+ *      Author: Anna Semenova
+ */
+#include <stdio.h>
+#include <stdio.h>
+
+int main (void)
+{
+    int a;
+    int b;
+    float res;
+    char i;
+
+
+}
+
